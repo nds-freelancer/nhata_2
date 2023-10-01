1cf781832cde358e05bf41ceb97d89dd08622dbe
D.Son <nds.freelancer@gmail.com>
2023-10-01 21:52:27 +0700

    RRREEE

---
+++ src/main/java/net/user/CakeController.java
@@ -0,0 +1,15 @@
+package net.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class CakeController {
+
+
+@RequestMapping("/home")
+public String home() {
+    return "index";
+}
+}
